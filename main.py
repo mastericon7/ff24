bda2e132184bb112774da94e8a970230e360c3a0
mastericon7 <bronzarena@gmail.com>
2023-11-11 18:41:26 +0100

    v0.0.6
    added packopening mechanism but not ready yet. todo: finish packopening

--- main.py
+++ main.py
@@ -32,14 +32,14 @@ def show_main_menu():
     frunning = True
 
     # Load PNG images for different background colors
-    background_1 = pygame.image.load('data/images/quickmatch.png').convert()
-    background_2 = pygame.image.load('data/images/careermatch.png').convert()
-    background_3 = pygame.image.load('data/images/ff24lobby.png').convert()
-    background_4 = pygame.image.load('data/images/quitmatch.png').convert()
+    background_1 = pygame.image.load('data/images/quickmatch.png').convert_alpha()
+    background_2 = pygame.image.load('data/images/careermatch.png').convert_alpha()
+    background_3 = pygame.image.load('data/images/ff24lobby.png').convert_alpha()
+    background_4 = pygame.image.load('data/images/quitmatch.png').convert_alpha()
 
     # Define transition variables
     transitioning = False
-    transition_duration = 50 # duration of transition
+    transition_duration = 35 # duration of transition
     transition_frame = 0
 
     while frunning:
@@ -123,13 +123,13 @@ def fusionteam():
     selected_option = 0
     trunning = True
     # Load PNG images for different background colors
-    background_1 = pygame.image.load('data/images/myteammenu.png').convert()
-    background_2 = pygame.image.load('data/images/marketmenu.png').convert()
-    background_3 = pygame.image.load('data/images/packsmenu.png').convert()
-    background_4 = pygame.image.load('data/images/quitmatch.png').convert()
+    background_1 = pygame.image.load('data/images/myteammenu.png').convert_alpha()
+    background_2 = pygame.image.load('data/images/marketmenu.png').convert_alpha()
+    background_3 = pygame.image.load('data/images/packsmenu.png').convert_alpha()
+    background_4 = pygame.image.load('data/images/quitmatch.png').convert_alpha()
     # Define transition variables
     transitioning = False
-    transition_duration = 50 # duration of transition
+    transition_duration = 35 # duration of transition
     transition_frame = 0
     while trunning:
         for event in pygame.event.get():
@@ -151,6 +151,7 @@ def fusionteam():
                         elif selected_option == 2:
                             packs()
                         elif selected_option == 3:
+                            trunning = False
                             show_main_menu()
                     # Start the transition
                     transitioning = True
@@ -202,16 +203,27 @@ def packs():
     global screen
 
     pygame.init()
+
     screen_width, screen_height = 1920, 1080
     screen = pygame.display.set_mode((screen_width, screen_height))
 
     menu_font = pygame.font.Font('data/fonts/justsansbold.otf', 36)
     menu_options = ['Go Back']
-    selected_option = 0
+    selected_option = 1
 
-    bluepack_image = pygame.image.load("data/images/bluepack80.png")
-    yellowpack_image = pygame.image.load("data/images/bluepack80.png")
-    redpack_image = pygame.image.load("data/images/bluepack80.png")
+    bluepack_image = pygame.image.load("data/images/bluepack80.png").convert_alpha()
+    goldpack_image = pygame.image.load("data/images/goldpack80.png").convert_alpha()
+    brownpack_image = pygame.image.load("data/images/brownpack80.png").convert_alpha()
+
+    image_width = 369
+    image_height = 469
+    image_margin = 20
+    num_options = 3
+    total_width = num_options * image_width + (num_options - 1) * image_margin
+    start_x = (screen_width - total_width) // 2
+    start_y = 500
+
+    target_y = start_y  # Initialize target position
 
     prunning = True
     while prunning:
@@ -219,27 +231,28 @@ def packs():
             if event.type == pygame.QUIT:
                 pygame.quit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_UP:
                     selected_option = (selected_option - 1) % (len(menu_options) + 3)
-                elif event.key == pygame.K_RIGHT:
+                    target_y -= 100  # Move the selected option higher
+                elif event.key == pygame.K_DOWN:
                     selected_option = (selected_option + 1) % (len(menu_options) + 3)
+                    target_y += 100  # Move the selected option lower
+                elif event.key == pygame.K_LEFT:
+                    selected_option = (selected_option - 1) % (num_options + 1)
+                    if selected_option != 0:
+                        target_y -= 50  # Move the selected pack higher
+                elif event.key == pygame.K_RIGHT:
+                    selected_option = (selected_option + 1) % (num_options + 1)
+                    if selected_option != 0:
+                        target_y -= 50  # Move the selected pack higher
                 elif event.key == pygame.K_RETURN:
                     if selected_option == 0:
                         # Start the game
                         fusionteam()
-                    elif selected_option == 1:
-                        # Start the game
-                        pass
-                    elif selected_option == 2:
-                        # Start the game
-                        pass
-                    elif selected_option == 3:
-                        # Start the game
-                        print('3')
 
         screen.fill((52, 52, 52))
 
-        menu_item_y = 200
+        menu_item_y = start_y - 80
         for i, option in enumerate(menu_options):
             color = (25, 120, 165) if i == selected_option else (240, 240, 240)
             text = menu_font.render(option, True, color)
@@ -247,27 +260,16 @@ def packs():
             screen.blit(text, text_rect)
             menu_item_y += 80
 
-        # Display selectable menu points from images next to each other
-        image_width = bluepack_image.get_width()
-        image_height = bluepack_image.get_height()
-        x_position = (screen_width - 3 * 369 - 2 * 20) // 2
-        if selected_option == 1:
-            screen.blit(pygame.transform.scale(bluepack_image, (369, 469)), (x_position, 500))
-            pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(x_position - 5, 495, 379, 479), 3)
-        else:
-            screen.blit(pygame.transform.scale(bluepack_image, (369, 469)), (x_position, 500))
-        x_position += 389
-        if selected_option == 2:
-            screen.blit(pygame.transform.scale(yellowpack_image, (369, 469)), (x_position, 500))
-            pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(x_position - 5, 495, 379, 479), 3)
-        else:
-            screen.blit(pygame.transform.scale(yellowpack_image, (369, 469)), (x_position, 500))
-        x_position += 389
-        if selected_option == 3:
-            screen.blit(pygame.transform.scale(redpack_image, (369, 469)), (x_position, 500))
-            pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(x_position - 5, 495, 379, 479), 3)
-        else:
-            screen.blit(pygame.transform.scale(redpack_image, (369, 469)), (x_position, 500))
+        # Display selectable menu points from images next to each other with animation
+        x_position = start_x
+        for i, image in enumerate([bluepack_image, goldpack_image, brownpack_image]):
+            if selected_option == i + 1:
+                pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(x_position - 5, target_y - 5, image_width + 10, image_height + 10), 3)
+            screen.blit(pygame.transform.scale(image, (image_width, image_height)), (x_position, target_y))
+            x_position += image_width + image_margin
+
+        # Smoothly adjust the target position
+        target_y += (start_y - target_y) * 0.1
 
         pygame.display.flip()
     pygame.quit()
@@ -278,8 +280,8 @@ pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 #images
-bg = pygame.image.load("data/images/FF24.png")
-bg2 = pygame.image.load("data/images/ff24loading.png")
+bg = pygame.image.load("data/images/FF24.png").convert_alpha()
+bg2 = pygame.image.load("data/images/ff24loading.png").convert_alpha()
 
 #fonts
 justsans = pygame.font.Font('data/fonts/justsansbold.otf', 40)
---
+++ packopening.py
@@ -0,0 +1,115 @@
+from random import randint
+def packmake80():
+    packopen80 = randint(1, 26)
+
+    if packopen80 == 1:
+        print('Linnea Bjorn - Sweden')
+    elif packopen80 == 2:
+        print('Pavel Sokolov - Czech Republic')
+    elif packopen80 == 3:
+        print('Anika Gupta - India')
+    elif packopen80 == 4:
+        print('Leonidas Papadopoulos - Greece')
+    elif packopen80 == 5:
+        print('Freya Jensen - Denmark')
+    elif packopen80 == 6:
+        print('Hiro Tanaka - Japan')
+    elif packopen80 == 7:
+        print('Anita Patel - Canada')
+    elif packopen80 == 8:
+        print('Matej Kolar - Croatia')
+    elif packopen80 == 9:
+        print('Isak Karlsson - Sweden')
+    elif packopen80 == 10:
+        print('Mia Andersson - Norway')
+    elif packopen80 == 11:
+        print('Arjun Rao - India')
+    elif packopen80 == 12:
+        print('Valentina Costa - Argentina')
+    elif packopen80 == 13:
+        print('Yuri Ivanov - Russia')
+    elif packopen80 == 14:
+        print('Irina Petrova - Bulgaria')
+    elif packopen80 == 15:
+        print('Lucas da Silva - Brazil')
+    elif packopen80 == 16:
+        print('Nadia Petrovic - Serbia')
+    elif packopen80 == 17:
+        print('Marco Hernandez - Spain')
+    elif packopen80 == 18:
+        print('Katarina Novak - Czech Republic')
+    elif packopen80 == 19:
+        print('Andrei Ionescu - Romania')
+    elif packopen80 == 20:
+        print('Elena Volkova - Ukraine')
+    elif packopen80 == 21:
+        print('David Alonso - Spain')
+    elif packopen80 == 22:
+        print('Ingrid Jorgensen - Norway')
+    elif packopen80 == 23:
+        print('Juan Martinez - Spain')
+    elif packopen80 == 24:
+        print('Lars Olsen - Denmark')
+    elif packopen80 == 25:
+        print('Maria Sanchez - Spain')
+    elif packopen80 == 26:
+        print('Ahmed Abadi - Egypt')
+
+def packmake70():
+    packopen70 = randint(1, 17)
+
+    if packopen70 == 1:
+        print('Alex Diamante - Argentina')
+    elif packopen70 == 2:
+        print('Maxime Roux - France')
+    elif packopen70 == 3:
+        print('Diego Fernandez - Spain')
+    elif packopen70 == 4:
+        print('Elena Costa - Brazil')
+    elif packopen70 == 5:
+        print('Andrei Petrov - Russia')
+    elif packopen70 == 6:
+        print('Sofia Kovač - Croatia')
+    elif packopen70 == 7:
+        print('Rafael Silva - Portugal')
+    elif packopen70 == 8:
+        print('Leila Hassan - Egypt')
+    elif packopen70 == 9:
+        print('Viktor Novak - Serbia')
+    elif packopen70 == 10:
+        print('Maya Chen - China')
+    elif packopen70 == 11:
+        print('Nikolai Volkov - Ukraine')
+    elif packopen70 == 12:
+        print('Carmen Morales - Mexico')
+    elif packopen70 == 13:
+        print('Matteo Bianchi - Italy')
+    elif packopen70 == 14:
+        print('Isabella Rojas - Colombia')
+    elif packopen70 == 15:
+        print('Lars Jansen - Netherlands')
+    elif packopen70 == 16:
+        print('Serena Montoya - Colombia')
+    elif packopen70 == 17:
+        print('Hassan Al-Mansur - Saudi Arabia')
+
+    def packmake90():
+        packopen90 = randint(1, 7)
+
+        if packopen90 == 1:
+            print('Astrid Larsen - Norway')
+        elif packopen90 == 2:
+            print('Sergei Romanov - Russia')
+        elif packopen90 == 3:
+            print('Aisha Malik - Pakistan')
+        elif packopen90 == 4:
+            print('Dimitri Kuznetsov - Russia')
+        elif packopen90 == 5:
+            print('Erika Lindstrom - Sweden')
+        elif packopen90 == 6:
+            print('Yuki Tanaka - Japan')
+        elif packopen90 == 7:
+            print('Sofia Rodriguez - Argentina')
+def packopening():
+    global packmake80, packmake70, packmake90
+    randint(packmake70, packmake90, packmake80)
